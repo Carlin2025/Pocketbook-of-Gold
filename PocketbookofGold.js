0a1ef16d0cf594b3a5a49c9ff8bd7cbd067303d9
Carlin2025 <aeronews24@protonmail.com>
2025-09-05 16:13:03 +0100

    Add files via upload

---
+++ PocketbookofGold.js
@@ -0,0 +1,18 @@
+
+/*console.log(document.querySelector('.IB').textContent);
+
+function button1(){
+    document.getElementById("button1").innerText = "Done!";
+}
+buttonsArray.Result= function(){
+    document.getElementById("button1").innerText ="Done!";
+}*/
+//Remember divs in HTML
+/*
+<div class="Bottom">
+        <form id="contact-form">
+            <button class="buttons", id="SendButton", type="submit">Send</button>
+        </form>
+    </div>
+*/
+//Set the heights to be the same for inner box and outer box.
